bcc20147e1a13c64f51d15b16a075ca5cbc0bb4f
idrisregg <idrisregg@gmail.com>
2025-12-03 17:05:56 +0100

    changed background rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react'
-import getRandomNumber  from '../service/calc.js'
+import getRandomNumber from '../service/calc.js'
 import './App.css'
 import Cards from '../comps/cards.jsx'
 
 function App() {
   const [combination, setCombination] = useState(getRandomNumber())
   const target = combination[0] * combination[1] * combination[2]
-  
+
   const [selectedCards, setSelectedCards] = useState([])
   const [timer, setTimer] = useState(20)
   const [gameActive, setGameActive] = useState(true)
@@ -15,17 +15,20 @@ function App() {
 
   // countdown timer effect
   useEffect(() => {
+    document.body.style.backgroundImage = "url('/bg.png')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "center";
     if (timer <= 0 && gameActive) {
       setGameActive(false)
       setMessage('Time\'s up! You Lose!')
     }
-    
+
     if (!gameActive) return
-    
+
     const interval = setInterval(() => {
       setTimer(t => t - 1)
     }, 1000)
-    
+
     return () => clearInterval(interval)
   }, [timer, gameActive])
 
@@ -62,12 +65,12 @@ function App() {
       <h2>Pick 3 Cards</h2>
       <div className='timer'>⏱️ {timer}s</div>
       <h3>Target: {target}</h3>
-      
+
       <div className='main'>
-        {[1,2,3,4,5,6,7,8,9,10].map(num => (
-          <Cards 
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(num => (
+          <Cards
             key={num}
-            num={num} 
+            num={num}
             onClick={() => handleCardClick(num)}
             locked={selectedCards.includes(num)}
             disabled={!gameActive}
